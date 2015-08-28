f428104b8912ed43df7f77ddce20224c904950bf
piglikeyoung <316046510@qq.com>
2015-08-28 14:04:34 +0800

    增加一个创建RGBA颜色的宏

--- mOne-iOS/Classes/Expand/Macros/AppMacro.h
+++ mOne-iOS/Classes/Expand/Macros/AppMacro.h
@@ -22,6 +22,7 @@
 
 // 颜色
 #define JHColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+#define JHColorRGBA(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
 
 // 是否为iOS7
 #define iOS7 ([[UIDevice currentDevice].systemVersion doubleValue] >= 7.0)
