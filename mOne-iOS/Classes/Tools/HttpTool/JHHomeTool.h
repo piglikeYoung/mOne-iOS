bb5beb59f5ac17a45dbe223b4ecd11eaa863fadc
piglikeyoung <316046510@qq.com>
2015-08-30 11:58:29 +0800

    修改注释

--- mOne-iOS/Classes/Tools/HttpTool/JHHomeTool.h
+++ mOne-iOS/Classes/Tools/HttpTool/JHHomeTool.h
@@ -15,9 +15,6 @@
 /**
  *  获取首页数据
  *
- *  @param index   要展示数据的 Item 的下标
- *  @param success 请求成功 Block
- *  @param fail    请求失败 Block
  */
 + (void)homeContentWithParam:(JHHomeParam *)param success:(void (^)(JHHomeResult *result))success failure:(void (^)(NSError *error))failure;
 
